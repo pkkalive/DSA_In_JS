d36423503ee9da476edbd1dc12c352e430f2fbff
Purushotham Kumar <kalivepurushothamkumar@gmail.com>
2022-07-24 15:46:23 +0530

    timeEquality

---
+++ Arrays/TimeEquality.js
@@ -0,0 +1,14 @@
+function timeEquality(A){
+  const len = A.length;
+  let max = A[0];
+  let ans = 0
+  for (let num of A){
+    max = Math.max(max, num);
+  }
+  for (let num of A){
+    ans += (max - num);
+  }
+  return ans;
+}
+
+console.log(timeEquality([2, 4, 1, 3, 2]));
